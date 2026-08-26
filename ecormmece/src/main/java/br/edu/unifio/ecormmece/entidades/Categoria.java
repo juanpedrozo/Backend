3734cede3b2734dc9b98dc2e24057899f8996f1a
Juan Pedrozo <pedrozojuan070@gmail.com>
2026-08-26 23:44:41 +0000

    Adiciona novas funcionalidades

--- ecormmece/src/main/java/br/edu/unifio/ecormmece/entidades/Categoria.java
+++ ecormmece/src/main/java/br/edu/unifio/ecormmece/entidades/Categoria.java
@@ -2,12 +2,20 @@ package br.edu.unifio.ecormmece.entidades;
 
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
 public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+
     private Short id; 
     private String nome;
     private String descricao;
